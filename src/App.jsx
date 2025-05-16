17cd3d517eafe5fee5d0e129a1bf258ea3c39dfc
Vansh Kumar Gupta <vg25574256@gmail.com>
2025-05-17 01:58:23 +0530

    code

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
+import Contact from './components/Contact'
+import { ToastContainer} from 'react-toastify';
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' w-full overflow-hidden'>
+        <ToastContainer/>
+        <Header/>
+        <About/>
+        <Projects/>
+        <Testimonails/>
+        <Contact/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
